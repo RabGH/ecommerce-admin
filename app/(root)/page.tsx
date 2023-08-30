fd55b63f2b12671031123c1aa6b3eee7d9e02b6d
RabGh <rabiighais@gmail.com>
2023-08-30 09:44:44 +0300

    Clerk sign up & sign in pop up pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="default" size="icon">Hello!</Button>;
+  return (
+    <div className="p-6">
+      <Button variant="default" size="default">
+        Hello!
+      </Button>
+    </div>
+  );
 }
