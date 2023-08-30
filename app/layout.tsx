21ed8e7e8686d5ab6c1d369b01188ad48aaf83fc
RabGh <rabiighais@gmail.com>
2023-08-30 09:44:23 +0300

    Add clerk initial auth & middle ware

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,25 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Admin Commerce Dashboard',
-  description: 'This is an admin e-commerce dashboard CMS and CRM.',
-}
+  title: "Admin Commerce Dashboard",
+  description: "This is an admin e-commerce dashboard CMS and CRM.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
 }
