2d054c30818208acb3e98c7193b85855e3eed501
RabGh <rabiighais@gmail.com>
2023-08-31 00:41:23 +0300

    Test API route & prisma database

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import axios from "axios"
+import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 
@@ -18,7 +18,6 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -39,7 +38,9 @@ export const StoreModal = () => {
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       setLoading(true);
-      const response = await axios.post('/api/stores', values)
+      const response = await axios.post("/api/stores", values);
+
+      console.log(response.data);
     } catch (error) {
       console.log(error);
     } finally {
