c250973e153c56f45fb6d266c42b93acf3b8b18a
RabGh <rabiighais@gmail.com>
2023-08-31 17:09:04 +0300

    Billboard form copied & modified store form

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -1,3 +1,4 @@
+import { BillboardForm } from "@/components/billboard-form";
 import prismadb from "@/lib/prismadb";
 
 const BillboardPage = async ({
@@ -11,7 +12,13 @@ const BillboardPage = async ({
     },
   });
 
-  return <div>Existing Billboard: {billboard?.label}</div>;
+  return (
+  <div className="flex-col">
+    <div className="flex-1 space-y-4 p-8 pt-6">
+        <BillboardForm initialData={billboard} />
+    </div>
+  </div>
+  );
 };
 
 export default BillboardPage;
