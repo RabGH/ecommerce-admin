5241f075e27ef177c90b71afd4d94e6679888096
RabGh <rabiighais@gmail.com>
2023-08-31 13:51:04 +0300

    Add delete store icon in settings form

--- components/settings-form.tsx
+++ components/settings-form.tsx
@@ -1,15 +1,24 @@
-"use client"
+"use client";
 
-import { Store } from "@prisma/client"
+import { Store } from "@prisma/client";
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 
 interface SettingsFormProps {
-    initialData: Store
+  initialData: Store;
 }
 
 export const SettingsForm: React.FC<SettingsFormProps> = () => {
-    return (
-        <div>
-            Settings Form
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading title="Settings" description="Manage store preferences" />
+        <Button variant="destructive" size="icon" onClick={() => {}}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </div>
+      
+    </>
+  );
+};
