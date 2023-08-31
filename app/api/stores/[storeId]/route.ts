8a0aafd9bcbbbc7a6344a783eef8be440d4ca79b
RabGh <rabiighais@gmail.com>
2023-08-31 22:28:26 +0300

    Add status 200 to success json request

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -31,7 +31,7 @@ export async function PATCH(
       },
     });
 
-    return NextResponse.json(store);
+    return NextResponse.json(store, { status: 200 });
   } catch (error) {
     console.log("[STORE_PATCH]", error);
     return new NextResponse("Internal error.", { status: 500 });
--- app/api/stores/route.ts
+++ app/api/stores/route.ts
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
       },
     });
 
-    return NextResponse.json(store);
+    return NextResponse.json(store, { status: 200 });
   } catch (error) {
     console.log("[STORES_POST]", error);
     return new NextResponse("Internal error.", { status: 500 });
