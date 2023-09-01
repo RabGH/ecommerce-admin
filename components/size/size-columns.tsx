f085ea0ebafe088f49963564cecdf5e378c66ed2
RabGh <rabiighais@gmail.com>
2023-09-01 19:40:13 +0300

    Fix mix up of size & color value

--- components/size/size-columns.tsx
+++ components/size/size-columns.tsx
@@ -19,15 +19,6 @@ export const columns: ColumnDef<SizeColumn>[] = [
   {
     accessorKey: "value",
     header: "Value",
-    cell: ({ row }) => (
-      <div className="flex items-center gap-x-2">
-        {row.original.value}
-        <div
-          className="h-6 w-6 rounded-full border"
-          style={{ backgroundColor: row.original.value }}
-        />
-      </div>
-    ),
   },
   {
     accessorKey: "createdAt",
