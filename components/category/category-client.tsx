3a06e183661a9b56451cbc13055abd80c4660b30
RabGh <rabiighais@gmail.com>
2023-09-01 06:10:22 +0300

    Category admin page UI copied from Billboard, temp

---
+++ components/category/category-client.tsx
@@ -0,0 +1,41 @@
+"use client";
+import { useParams, useRouter } from "next/navigation";
+import { Plus } from "lucide-react";
+
+import { Heading } from "@/components/ui/heading";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { CategoryColumn } from "@/components/category/category-columns";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "@/components/category/category-columns";
+import { ApiList } from "@/components/ui/api-list";
+
+interface CategoryClientProps {
+  data: CategoryColumn[];
+}
+
+export const CategoryClient: React.FC<CategoryClientProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Category (${data.length})`}
+          description="Manage categories for your store."
+        />
+        <Button
+          onClick={() => router.push(`/${params.storeId}/categories/new`)}
+        >
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="categories" entityIdName="categoryId" />
+    </>
+  );
+};
