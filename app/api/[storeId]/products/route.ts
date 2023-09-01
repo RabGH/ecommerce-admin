4007c4c7f848ae2166a290b5926d6f4d4e18973f
RabGh <rabiighais@gmail.com>
2023-09-01 22:43:16 +0300

    Product API route

--- app/api/[storeId]/products/route.ts
+++ app/api/[storeId]/products/route.ts
@@ -10,16 +10,37 @@ export async function POST(
   try {
     const { userId } = auth();
     const body = await req.json();
-    const { label, imageUrl } = body;
+    const {
+      name,
+      price,
+      categoryId,
+      colorId,
+      sizeId,
+      images,
+      isFeatured,
+      isArchived,
+    } = body;
 
     if (!userId) {
       return new NextResponse("Unauthenticated", { status: 403 });
     }
-    if (!label) {
-      return new NextResponse("Label is required", { status: 400 });
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
     }
-    if (!imageUrl) {
-      return new NextResponse("Image URL is required", { status: 400 });
+    if (!price) {
+      return new NextResponse("Price is required", { status: 400 });
+    }
+    if (!colorId) {
+      return new NextResponse("Color ID is required", { status: 400 });
+    }
+    if (!sizeId) {
+      return new NextResponse("Size ID is required", { status: 400 });
+    }
+    if (!categoryId) {
+      return new NextResponse("Category ID is required", { status: 400 });
+    }
+    if (!images || !images.length) {
+      return new NextResponse("Images are required", { status: 400 });
     }
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
@@ -39,8 +60,18 @@ export async function POST(
     const product = await prismadb.product.create({
       data: {
         name,
-        imageUrl,
+        price,
+        isArchived,
+        isFeatured,
+        categoryId,
+        sizeId,
+        colorId,
         storeId: params.storeId,
+        images: {
+          createMany: {
+            data: [...images.map((image: { url: string }) => image)],
+          },
+        },
       },
     });
 
@@ -56,6 +87,12 @@ export async function GET(
   { params }: { params: { storeId: string } }
 ) {
   try {
+    const { searchParams } = new URL(req.url);
+    const categoryId = searchParams.get("categoryId") || undefined;
+    const colorId = searchParams.get("colorId") || undefined;
+    const sizeId = searchParams.get("sizeId") || undefined;
+    const isFeatured = searchParams.get("isFeatured");
+
     if (!params.storeId) {
       return new NextResponse("Store id is required", { status: 400 });
     }
@@ -63,6 +100,20 @@ export async function GET(
     const products = await prismadb.product.findMany({
       where: {
         storeId: params.storeId,
+        categoryId,
+        colorId,
+        sizeId,
+        isFeatured: isFeatured ? true : undefined,
+        isArchived: false,
+      },
+      include: {
+        images: true,
+        category: true,
+        color: true,
+        size: true,
+      },
+      orderBy: {
+        createdAt: "desc",
       },
     });
 
