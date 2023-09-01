20d7f756ce43d7414c8882e276fa5b6437fc55d3
RabGh <rabiighais@gmail.com>
2023-09-01 04:26:16 +0300

    Add API alert list for billboards & component

--- components/misc/client.tsx
+++ components/misc/client.tsx
@@ -8,6 +8,7 @@ import { Separator } from "@/components/ui/separator";
 import { BillboardColumn } from "@/components/misc/columns";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "@/components/misc/columns";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -32,6 +33,9 @@ export const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
       </div>
       <Separator />
       <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for Billboards" />
+      <Separator />
+      <ApiList entityName="billboards" entityIdName="billboardId" />
     </>
   );
 };
